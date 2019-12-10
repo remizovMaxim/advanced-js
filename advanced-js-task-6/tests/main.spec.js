50bc2f77f85665182c3a382a4cccd78570df3a31
Maxim <sharkercool96@gmail.com>
2019-12-10 13:43:53 +0300

    change test

--- advanced-js-task-6/tests/main.spec.js
+++ advanced-js-task-6/tests/main.spec.js
@@ -1,16 +1,5 @@
 const getStrTime = require('../src/main');
 
-// describe('11 Test function', () => {
-//     it('Should be "your paramet is not number:("', () => {
-//         expect(getStrTime('exclusive')).toEqual("your paramet is not number:(");
-//     });
-    
-//     it('Should be "called"', () => {
-//         expect(getStrTime(2000)).toEqual('called');
-//         expect(getStrTime(2000)).toBeUndefined();
-//     });
-// });
-
 describe('Test function getAtrTime', () => {
     it('Should be "your paramet is not number:("', () => {
         expect(getStrTime('exclusive')).toEqual("your paramet is not number:(");
@@ -18,7 +7,7 @@ describe('Test function getAtrTime', () => {
     
     it('Should be "called"', () => {
         let test1 = getStrTime(2000).split(' time: ');
-        
+
         expect(test1[0]).toEqual('called');
         expect(getStrTime(2000)).toBeUndefined();
     });
